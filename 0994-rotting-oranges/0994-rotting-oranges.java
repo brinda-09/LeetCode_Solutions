c54ce6052602ec3bbdbb135fdac90ebfab7b5771
Brinda D <brinda220906@gmail.com>
2026-03-25 09:41:57 +0530

    Time: 1 ms (99.98%), Space: 43.9 MB (88.57%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        int rows=grid.length;
+        int cols=grid[0].length;
+        Queue<int[]> q=new ArrayDeque<>();
+        int fresh=0;
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(grid[i][j]==2){
+                    q.offer(new int[]{i,j});
+                }
+                if(grid[i][j]==1)
+                fresh++;
+            }
+        }
+        int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
+        int minute=0;
+        while (!q.isEmpty()&&fresh>0) {
+            int size=q.size();
+            for(int i=0;i<size;i++){
+            int[] cur = q.poll();
+            int r = cur[0], c = cur[1];
+            for (int[] d : dir) {
+                int nr = r + d[0];
+                int nc = c + d[1];
+
+                if (nr >= 0 && nr < rows && nc >= 0 && nc < cols && grid[nr][nc] == 1) {
+                    grid[nr][nc] = 2;
+                    fresh--;
+                    q.offer(new int[]{nr, nc});
+                }}
+            }
+            minute++;
+        }
+
+        if(fresh== 0 )
+        return minute ;
+        return -1;
+    }
+}
